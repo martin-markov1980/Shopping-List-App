c816ef9250088873102a769de429214695a84bc4
Martin Markov <martin.markov@weareaccess.co.uk>
2021-11-23 22:39:35 +0000

    Added add task and delete all tasks functionality.

---
+++ script.js
@@ -0,0 +1,99 @@
+
+let addTaskBtn = document.getElementById('add-task');
+let addTaskBtnModal = document.getElementById('add-task-btn-modal');
+let addItemModal = document.getElementById('add-item-modal');
+let tableBody = document.querySelector('tbody')
+let taskName = document.getElementById('task-name');
+let taskNameValue;
+let quantity = document.getElementById('task-quantity');
+let quantityValue;
+let itemNameChecker = document.getElementById('item-name-checker');
+let taskQuantityChecker = document.getElementById('task-quantity-checker');
+let deleteAllTasksBtn = document.getElementById('delete-all-yes-modal');
+
+// Add Task Event
+addTaskBtnModal.addEventListener('click', addTask);
+
+// Delete All Tasks Event
+deleteAllTasksBtn.addEventListener('click', deleteAllTasks);
+
+// Disabled Add Task Button to prevent sending empty task name and quantity values
+addTaskBtn.addEventListener('click', () => {
+    let att = document.createAttribute('disabled');
+    addTaskBtnModal.setAttributeNode(att);
+})
+
+// Checking if task name and quantity have values and enabling the Add Task Button if they DO
+addItemModal.addEventListener('keyup', () => {
+    if (taskName.value.trim() !== ''  && quantity.value.trim() !== '') {
+        let att = addTaskBtnModal.getAttributeNode('disabled');
+        if (att) {
+            addTaskBtnModal.removeAttributeNode(att);
+        }
+    }   else {
+        let att = document.createAttribute('disabled');
+        addTaskBtnModal.setAttributeNode(att);
+    }
+})
+
+// Get the tasks from the localStorage. Build and display them
+tableBody.innerHTML = '';
+for (let index = 0; index < localStorage.length; index++) {
+        let itemName = localStorage.key(index);
+        let itemQuantity = localStorage.getItem(itemName);
+
+        let tr = document.createElement('tr');
+        let tdItem = document.createElement('td');
+        let btn = document.createElement('button');
+        btn.className ='btn btn-danger btn-lg';
+        btn.innerHTML = 'X'
+        tdItem.innerHTML = itemName;
+        let tdQuantity = document.createElement('td');
+        tdQuantity.innerHTML = itemQuantity;
+        let tdButton = document.createElement('td');
+        tdButton.appendChild(btn);
+
+        tr.appendChild(tdItem);
+        tr.appendChild(tdQuantity);
+        tr.appendChild(tdButton);
+
+        tableBody.appendChild(tr);
+    
+}
+
+// Add Task To Local Storage 
+function addTask() {
+    localStorage.setItem(`${taskName.value}`, `${quantity.value}`);
+    taskName.value = '';
+    quantity.value = '';
+    tableBody.innerHTML = '';
+
+    for (let index = 0; index < localStorage.length; index++) {
+        let itemName = localStorage.key(index);
+        let itemQuantity = localStorage.getItem(itemName);
+    
+        let tr = document.createElement('tr');
+        let tdItem = document.createElement('td');
+        let btn = document.createElement('button');
+        btn.className ='btn btn-danger btn-lg';
+        btn.innerHTML = 'X'
+        tdItem.innerHTML = itemName;
+        let tdQuantity = document.createElement('td');
+        tdQuantity.innerHTML = itemQuantity;
+        let tdButton = document.createElement('td');
+        tdButton.appendChild(btn);
+
+        tr.appendChild(tdItem);
+        tr.appendChild(tdQuantity);
+        tr.appendChild(tdButton);
+
+        tableBody.appendChild(tr);
+        
+    }
+}
+
+// Delete All Tasks
+function deleteAllTasks() {
+    localStorage.clear();
+    tableBody.innerHTML = '';
+}
