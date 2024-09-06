0b27dd58df19b7bd359d10784ebfdf3a17b6e4f5
Martin Markov <martin.markov1980@gmail.com>
2024-09-07 00:34:33 +0100

    Added script to run the Playwright tests.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,25 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Shopping List/);
 });
 
+test('has added todo', async ({ page }) => {
+  await page.goto('https://martin-markov1980.github.io/Shopping-List-App/');
+
+  // Click the button by its ID
+  await page.click('#add-task');
+
+  // Fill the input field by its selector
+  await page.fill('#task-name', 'Apple');
+
+  // Fill the input field by its selector
+  await page.fill('#task-quantity', '3');
+
+  const tableLocator = page.locator('table');
+  await expect(tableLocator).toHaveText(/Apple/);
+
+  // // Expect a title "to contain" a substring.
+  // await expect(page).toHaveTitle(/Shopping List/);
+});
+
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
 
